6c56fa06a5f6093ce175782210c9a9e39b38ff98
YUSUF EMRE DENİZ <yusufemredenix@outlook.com>
2025-03-26 21:55:31 +0300

    PremierLig Sayfası eklendi, Puan durumu vs., AnaSayfa düzenlendi

--- FutbolSitesi/Global.asax.cs
+++ FutbolSitesi/Global.asax.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-using System.Web.Http;
 
-namespace FutbolSitesi
+namespace FutbolSitesi // Namespace ile Global.asax içindeki adın aynı olmalı!
 {
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        protected void Application_Start()
         {
-            // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
---
+++ FutbolSitesi/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutbolSitesi.Models
+{
+    public class Team
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public List<Player> players { get; set; }
+    }
+}
